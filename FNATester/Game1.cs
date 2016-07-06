00d6209c06118f97ade8fafdde25b9b06b4b5716
Mike <mike@prime31.com>
2016-07-05 21:59:09 -0700

    tester csproj added

---
+++ FNATester/Game1.cs
@@ -0,0 +1,15 @@
+﻿using Nez;
+
+
+namespace FNATester
+{
+	public class Game1 : Core
+	{
+		protected override void Initialize()
+		{
+			base.Initialize();
+			scene = new TestScene();
+		}
+	}
+}
+
